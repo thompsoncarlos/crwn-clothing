f2d81d16ce6da1bde66cf3caea84231d29c1c7f2
Carlos Thompson <carlosethompson@gmail.com>
2024-05-18 10:18:16 +0100

    added in our checkouItem

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
+import CheckOut from "./routes/checkout/checkout.component";
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<Authentication />} />
+        <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<CheckOut />} />
       </Route>
     </Routes>
   );
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,24 +1,29 @@
-import { useContext } from "react";
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { CartContext } from "../../context/cart.context";
+import { CartContext } from '../../context/cart.context';
 
-import Button from "../button/button.component";
+import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-import CartItem from "../cart-item/cart-item.component";
-
-import "./cart-dropdown.styles.scss";
+import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  };
 
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
+    <div className='cart-dropdown-container'>
+      <div className='cart-items'>
         {cartItems.map((item) => (
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
